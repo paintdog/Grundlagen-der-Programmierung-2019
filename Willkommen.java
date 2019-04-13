cb22d589b57494d9db72800b5bdc5939c503079d
paintdog <paintdog@users.noreply.github.com>
2019-04-13 19:19:56 +0200

    Add files via upload

---
+++ Willkommen.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+/**
+ * Ausdruck von 2 Zeilen in einem Dialogfenster
+ * 
+ * 1. Version des Skripts, 2019, S. 9
+ */
+
+public class Willkommen {
+  
+  public static void main (String[] args) {
+    
+    JOptionPane.showMessageDialog(null, "Willkommen zur\nJava Programmierung!");
+  
+    }  
+}
