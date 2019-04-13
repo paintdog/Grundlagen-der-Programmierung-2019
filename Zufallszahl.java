cdf1f319cf49fedc6e21a3b6b6b04987595c875a
paintdog <paintdog@users.noreply.github.com>
2019-04-13 20:59:23 +0200

    Add files via upload

---
+++ Zufallszahl.java
@@ -0,0 +1,25 @@
+/* 
+ * Generierung einer Zufallszahl mittels Math.random()
+ *  
+ * 1. Version des Skripts, 2019, S. 16-17
+ * 
+ */
+
+public class Zufallszahl {
+  
+  public static void main (String[] args) {
+    
+    // Liefert eine Pseudozufallszahl z mit 0 <= z < 1
+    double z = Math.random();
+    System.out.println("Zufallszahl: " + z);
+    
+    // Zufallszahl zwischen 1 und 25
+    int Min =  1;
+    int Max = 25;
+      
+    z = Min + (int)(Math.random() * ((Max - Min) + 1));
+      
+    System.out.println("Zufallszahl: " + z);
+    
+    }  
+}
