e577a38fb47c3e44e59b9398e940d78643905ce6
paintdog <paintdog@users.noreply.github.com>
2019-04-13 19:41:55 +0200

    Add files via upload

---
+++ Dialogfenster.java
@@ -0,0 +1,29 @@
+import javax.swing.JOptionPane;
+
+/**
+ * Dialogfenster mi anderen Icons
+ * 
+ * 1. Version des Skripts, 2019, S. 15
+ */
+
+public class Dialogfenster {
+  
+  public static void main (String[] args) {
+    
+    // Medung ohne Icon (-1)
+    JOptionPane.showMessageDialog(null, "Willkommen zur\nJava Programmierung!", "Titel", JOptionPane.PLAIN_MESSAGE);
+    
+    // Felermeldung (0)
+    JOptionPane.showMessageDialog(null, "Willkommen zur\nJava Programmierung!", "Titel", JOptionPane.ERROR_MESSAGE);
+    
+    // informative Meldung (1)
+    JOptionPane.showMessageDialog(null, "Willkommen zur\nJava Programmierung!", "Titel", JOptionPane.INFORMATION_MESSAGE);
+    
+    // Warnmeldung (2)
+    JOptionPane.showMessageDialog(null, "Willkommen zur\nJava Programmierung!", "Titel", JOptionPane.WARNING_MESSAGE);
+    
+    // Frgemeldung (3)
+    JOptionPane.showMessageDialog(null, "Willkommen zur\nJava Programmierung!", "Titel", JOptionPane.QUESTION_MESSAGE);
+        
+    }  
+}
